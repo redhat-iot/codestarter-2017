c353a3178b6b2c0cdd6931c13a80f92ce0d55d49
jamesfalkner <schtool@gmail.com>
2017-04-28 09:48:35 -0400

    add Kapua API usage to JavaScript example

--- js/config.js
+++ js/config.js
@@ -3,6 +3,8 @@ var config =
     // hostname/port/name/password/topic for Kapua broker
     BROKER_HOSTNAME: process.env.BROKER_HOSTNAME || "broker_hostname",
     BROKER_PORT: process.env.BROKER_PORT || 80,
+    API_HOSTNAME: process.env.API_HOSTNAME || "api_hostname",
+    API_PORT: process.env.API_PORT || 80,
     BROKER_USERNAME: process.env.BROKER_USERNAME || "broker_username",
     BROKER_PASSWORD: process.env.BROKER_PASSWORD || "broker_password",
     BROKER_TOPIC_PREFIX: process.env.BROKER_TOPIC_PREFIX || "Some-Project/Some-Topic-Prefix"
--- js/views/utils.js
+++ js/views/utils.js
@@ -27,6 +27,72 @@ var UTILS = function(u) {
             s4() + '-' + s4() + s4() + s4();
     };
 
+    u.kapuaAuth = function(baseUrl, username, password, onSuccess, onFail) {
+        var httpRequest = new XMLHttpRequest();
+        var path = baseUrl + '/v1/authentication/user';
+        httpRequest.onreadystatechange = function () {
+            if (httpRequest.readyState === 4) {
+                if (httpRequest.status === 200) {
+                    var data = JSON.parse(httpRequest.responseText);
+                    if (onSuccess) onSuccess(data);
+                } else {
+                    if (onFail) {
+                        onFail("Error calling Authentication API " + path + ": " + httpRequest.status + " " + httpRequest.statusText);
+                    }
+                }
+            }
+        };
+
+        httpRequest.open('POST', path);
+        httpRequest.setRequestHeader("Content-type", "application/json");
+
+        var data = {
+            password: [
+                password
+            ],
+            username: username
+        };
+
+        httpRequest.send(JSON.stringify(data));
+    };
+
+    u.kapuaApi = function(baseUrl, apiPath, method, queryParams, body, auth, onSuccess, onFail) {
+        var httpRequest = new XMLHttpRequest();
+        httpRequest.onreadystatechange = function () {
+            if (httpRequest.readyState === 4) {
+                if (httpRequest.status === 200) {
+                    var data = JSON.parse(httpRequest.responseText);
+                    if (onSuccess) onSuccess(data);
+                } else {
+                    if (onFail) {
+                        onFail("Error calling API " + method + " " + apiPath + ": " + httpRequest.status + " " + httpRequest.statusText);
+                    }
+                }
+            }
+        };
+        var path = baseUrl + apiPath;
+        if (queryParams) {
+            path += '?';
+            Object.keys(queryParams).forEach(function(key, idx, arr) {
+                path += (key + '=' + encodeURIComponent(queryParams[key]));
+                if (idx < (arr.length - 1)) {
+                    path += '&';
+                }
+            });
+        }
+        httpRequest.open(method, path);
+        if (auth) {
+            httpRequest.setRequestHeader('Authorization', 'Bearer ' + auth.tokenId);
+        }
+        if (body) {
+            httpRequest.setRequestHeader("Content-type", "application/json");
+            httpRequest.send(JSON.stringify(body));
+        } else {
+            httpRequest.send();
+        }
+
+    };
+
     return u;
 }(UTILS || {});
 
